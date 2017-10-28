26cd5c56d7f3963a45dbf6ce26507ad4e62c9b7b
Sigma88 <sigmamodexpansions@outlook.com>
2017-10-28 16:06:39 +0200

    add usegameseed

--- [Source]/SigmaKerbalDescriptions/Extensions.cs
+++ [Source]/SigmaKerbalDescriptions/Extensions.cs
@@ -5,13 +5,15 @@ namespace SigmaKerbalDescriptions
 {
     internal static class Extensions
     {
-        internal static int Hash(this ProtoCrewMember kerbal)
+        internal static int Hash(this ProtoCrewMember kerbal, bool useGameSeed)
         {
             string hash = Information.hash;
 
             if (string.IsNullOrEmpty(hash)) hash = kerbal.name;
 
             int h = Math.Abs(hash.GetHashCode());
+
+            if (useGameSeed) h += Math.Abs(HighLogic.CurrentGame.Seed);
 
             Information.hash = h.ToString();
 
